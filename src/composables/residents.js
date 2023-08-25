8ef9c0c9ac085a91cecd442609df6b6c84e5d085
= <=>
2023-08-25 09:50:43 +0800

    Added ui for residents index

--- src/composables/residents.js
+++ src/composables/residents.js
@@ -2,15 +2,26 @@ import axios from "axios";
 import { ref } from "vue";
 import Swal from "sweetalert2";
 import { useRouter } from "vue-router";
+
 export default function useResidents() {
+  axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/residents/";
   const errors = ref({});
   const router = useRouter();
+  const residents = ref({});
+  const address = ref({});
+
+  const indexResidents = async (id) => {
+    try {
+      const response = await axios.get(id + "/residents");
+      residents.value = response.data.residents;
+      address.value = response.data.address;
+      console.log(response.data.house_number);
+    } catch (error) {}
+  };
+
   const storeResident = async (data) => {
     try {
-      const response = await axios.post(
-        "http://127.0.0.1:8000/api/v1/residents/store",
-        data
-      );
+      const response = await axios.post("store", data);
       if (response.status == 201) {
         await router.push({ name: "ResidentsIndexView" });
         await Swal.fire({
@@ -28,6 +39,9 @@ export default function useResidents() {
     }
   };
   return {
+    indexResidents,
+    residents,
+    address,
     storeResident,
     errors,
   };
